8ef47b43998fc9a34e7a6ede63ec702543b24913
jjang-occ <54513815+jjang-occ@users.noreply.github.com>
2019-08-25 15:47:57 -0700

    Update Test.java

--- HelloForGit/src/Test.java
+++ HelloForGit/src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello! This is my first Eclipse program maintained in git!");
 		
-		System.out.println("This line of code was edited in GitHub.");
+		System.out.println("This line of code was edited in GitHub and pulled into Eclipse.");
 	}
 
 }
