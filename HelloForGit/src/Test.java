f8b557eb118bb0321feebf1e919f5a3d09039175
justi <justi@JJ-XPS13-LAPTOP.home>
2019-08-25 15:32:11 -0700

    Added the Test class

---
+++ HelloForGit/src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! This is my first Eclipse program maintained in git!");
+	}
+
+}
